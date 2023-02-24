3c0a19301ec113db1e695c73a3637beb2609f5e5
MineGamer69 <aaryankapoor2003@gmail.com>
2023-02-24 15:44:14 -0500

    Game Finished

--- app/src/main/java/edu/quinnipiac/fourinarow/GameFragment.kt
+++ app/src/main/java/edu/quinnipiac/fourinarow/GameFragment.kt
@@ -1,21 +1,117 @@
 package edu.quinnipiac.fourinarow
 
+import FourInARow
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.graphics.toColor
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 
 
-class GameFragment : Fragment() {
+class GameFragment : Fragment(),View.OnClickListener {
 
+    val btn_Array = arrayOf(
+        R.id.button1,
+        R.id.button2,
+        R.id.button3,
+        R.id.button4,
+        R.id.button5,
+        R.id.button6,
+        R.id.button7,
+        R.id.button8,
+        R.id.button9,
+        R.id.button10,
+        R.id.button11,
+        R.id.button12,
+        R.id.button13,
+        R.id.button14,
+        R.id.button15,
+        R.id.button16,
+        R.id.button17,
+        R.id.button18,
+        R.id.button19,
+        R.id.button20,
+        R.id.button21,
+        R.id.button22,
+        R.id.button23,
+        R.id.button24,
+        R.id.button25,
+        R.id.button26,
+        R.id.button27,
+        R.id.button28,
+        R.id.button29,
+        R.id.button30,
+        R.id.button31,
+        R.id.button32,
+        R.id.button33,
+        R.id.button34,
+        R.id.button35,
+        R.id.button36
+    )
+
+    val listner = arrayListOf<Button>()
+    lateinit var uName: String
+    lateinit var NavCtrl: NavController
+    var FourInARow = FourInARow()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val valBund = arguments
+        if(valBund == null){
+            return
+        }
+        uName = GameFragmentArgs.fromBundle(valBund).userName.toString()
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        val view = inflater.inflate(R.layout.fragment_game, container, false)
+
+        for(i in 0 until btn_Array.size){
+            listner.add(view.findViewById<Button>(btn_Array[i]))
+        }
+        for(i in 0 until listner.size){
+            listner.get(i).setOnClickListener(this)
+        }
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        NavCtrl = Navigation.findNavController(view)
+        view.findViewById<TextView>(R.id.userID).text = uName
+    }
+
+    override fun onClick(p0: View?) {
+        for(i in 0 until listner.size){
+            val onClickDone: Button = view?.findViewById(btn_Array[i])!!
+            if(onClickDone == p0){
+                onClickDone.setBackgroundColor(Color.RED)
+                FourInARow.setMove(1, i)
+            }
+        }
+        val comMove = FourInARow.computerMove
+        FourInARow.setMove(2, comMove)
+        listner.get(comMove).setBackgroundColor(Color.BLUE)
+        FourInARow.checkForWinner()
+        if(FourInARow.checkForWinner() == 3){
+            Toast.makeText(context,uName + " has Won!", Toast.LENGTH_SHORT).show()
+        }
+        if(FourInARow.checkForWinner() == 2){
+            Toast.makeText(context, "Computer has Won!", Toast.LENGTH_SHORT).show()
+        }
+
     }
 
 }
+
--- app/src/main/java/edu/quinnipiac/fourinarow/StartFragment.kt
+++ app/src/main/java/edu/quinnipiac/fourinarow/StartFragment.kt
@@ -1,28 +1,40 @@
 package edu.quinnipiac.fourinarow
 
 import android.os.Bundle
+import android.text.TextUtils
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.TextView
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 
 
-class StartFragment : Fragment() {
+class StartFragment : Fragment(), View.OnClickListener{
+    lateinit var NavCtrl: NavController
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         //adding start buttons
-        val view = inflater.inflate(R.layout.fragment_start, container, false)
-        val startButton = view.findViewById<Button>(R.id.startBut)
-        val UserN = view.findViewById<TextView>(R.id.PersonName)
+        return inflater.inflate(R.layout.fragment_start, container, false)
+    }
 
-        startButton.setOnClickListener {
-            view.findNavController()
-                .navigate(R.id.action_startFragment_to_gameFragment)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        NavCtrl = Navigation.findNavController(view)
+        view.findViewById<Button>(R.id.startBut).setOnClickListener(this)
+    }
+    override fun onClick(v: View?){
+        val usName: EditText = requireView().findViewById(R.id.PersonName)
+        if(!TextUtils.isEmpty(usName.text.toString())){
+            val uPlayer = usName.text.toString()
+            val doAction = StartFragmentDirections.actionStartFragmentToGameFragment(uPlayer)
+            v?.findNavController()?.navigate(doAction)
         }
-        return view
+
     }
 }
